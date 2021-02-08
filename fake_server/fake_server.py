5110fd9d9bec09ec13aee3e879ba5d3720885b2b
Айрат Мухаметгараев <airat9753@gmail.com>
2021-02-08 21:48:27 +0300

    Первый вариант фейкового сервера

---
+++ fake_server/fake_server.py
@@ -0,0 +1,71 @@
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+
+@routes.get('/auth')
+async def auth(request):
+    """
+    Returns fake authorization token
+    """
+    login = request.rel_url.query['login']
+    password = request.rel_url.query['password']
+
+    response = {
+        "status": "OK",
+        "token": "dsfd79843r32d1d3dx23d32d"
+    }
+
+    return web.json_response(response)
+
+
+@routes.get('/get-user/{username}')
+async def get_user(request):
+    """
+    Returns fake user info
+    """
+    token = request.rel_url.query['token']
+
+    response = {
+        "status": "OK",
+        "active": "1",
+        "blocked": False,
+        "created_at": 1587457590,
+        "id": 23,
+        "name": "Ivanov Ivan",
+        "permissions": [
+            {
+                "id": 1,
+                "permission": "comment"
+            },
+            {
+                "id": 2,
+                "permission": "upload photo"
+            },
+            {
+                "id": 3,
+                "permission": "add event"
+            }
+        ]
+    }
+
+    return web.json_response(response)
+
+
+@routes.post('/user/{userid}/update')
+async def update_user(request):
+    """
+    Update user info
+    """
+    token = request.rel_url.query['token']
+
+    response = {
+        "status": "OK",
+    }
+
+    return web.json_response(response)
+
+
+app = web.Application()
+app.add_routes(routes)
+web.run_app(app)
